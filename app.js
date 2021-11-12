5f0c616c9e2e9e1877db3b6dbaf87d9b8ab8bcc0
Giuseppe Falcidia <giuseppefalcidia.mail@gmail.com>
2021-11-12 15:28:24 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser  = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json);
+
+const dotenv = require('dotenv');
+
+
+const connectDB = require('./config/db');
+
+// Load Config
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+// Routes
+app.use('/', require('./routes/index'));
+
+app.listen(3000);
